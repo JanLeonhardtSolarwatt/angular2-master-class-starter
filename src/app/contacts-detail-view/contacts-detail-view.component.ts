8656a41ca5e2d8a5e2837b059f8370009a2e2682
Jan Leonhardt <jan.leonhardt@solarwatt.net>
2016-10-27 14:24:59 +0200

    added presentation view

---
+++ src/app/contacts-detail-view/contacts-detail-view.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {ContactsService} from "../contacts.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Contact} from "../models/contact";
+
+@Component({
+    selector: 'trm-contacts-detail-view',
+    templateUrl: './contacts-detail-view.component.html',
+    styleUrls: ['./contacts-detail-view.component.css']
+})
+export class ContactsDetailViewComponent implements OnInit {
+    contact: Contact;
+
+    constructor(private contactsService: ContactsService,
+                private route: ActivatedRoute,
+                private router: Router) {
+    }
+
+    ngOnInit() {
+        let id = this.route.snapshot.params['id'];
+        this.contactsService.getContact(id)
+            .subscribe(contact => this.contact = contact);
+    }
+
+    editContact(contact: Contact) {
+        this.router.navigate(['/contact', contact.id,'edit']);
+    }
+    backToList() {
+        this.router.navigate(['/']);
+    }
+}
--- src/app/contacts-detail/contacts-detail.component.ts
+++ src/app/contacts-detail/contacts-detail.component.ts
@@ -1,26 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ContactsService } from '../contacts.service';
-import { Contact } from '../models/contact';
+import {Component, Output, EventEmitter, Input} from '@angular/core';
+import {Contact} from '../models/contact';
 
 @Component({
-  selector: 'trm-contacts-detail',
-  templateUrl: './contacts-detail.component.html',
-  styleUrls: ['./contacts-detail.component.css']
+    selector: 'trm-contacts-detail',
+    templateUrl: './contacts-detail.component.html',
+    styleUrls: ['./contacts-detail.component.css']
 })
-export class ContactsDetailComponent implements OnInit {
+export class ContactsDetailComponent {
 
-  contact: Contact;
+    @Input() contact: Contact;
+    @Output() edit = new EventEmitter<Contact>();
+    @Output() back = new EventEmitter();
 
-  constructor(
-    private contactsService: ContactsService,
-    private route: ActivatedRoute
-  ) { }
-
-  ngOnInit() {
-    let id = this.route.snapshot.params['id'];
-    this.contactsService.getContact(id)
-      .subscribe(contact => this.contact = contact);
-  }
-
+    constructor() {
+    }
 }
